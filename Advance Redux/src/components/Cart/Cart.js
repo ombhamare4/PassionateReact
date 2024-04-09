437b802cae9abecafde1c8fc38d7537134730d27
Om Ghanshyam Bhamare <om.rubiks@gmail.com>
2024-04-09 09:13:57 +0000

    Added Cloud Database and useffect to store data in db

--- Advance Redux/src/components/Cart/Cart.js
+++ Advance Redux/src/components/Cart/Cart.js
@@ -6,7 +6,6 @@ import CartItem from "./CartItem";
 
 const Cart = (props) => {
   const cart = useSelector((state) => state.cart);
-  console.log(cart);
   return (
     <Card className={classes.cart}>
       <h2>Your Shopping Cart</h2>
